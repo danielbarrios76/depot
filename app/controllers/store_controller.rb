6c290d4069728d74e43dce079990ee5ccf52908e
danielbarrios76 <danielbarrios76@hotmail.com>
2018-01-19 14:33:09 -0300

    Actualizacion

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,20 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    #show load counter messages
+    counter = get_loader_counter
+    @show_counter = nil
+    if counter > 5
+      @show_counter = "Session: #{counter} " + 'time'.pluralize
+    end
+
   end
+
+  protected
+  def get_loader_counter
+    session[:counter] ||= 0
+    return session[:counter] += 1
+  end
+
 end
